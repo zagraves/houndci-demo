37579f7e05fdb2f0672ba0e16910838641a7d91c
Zach Graves <zagraves@gmail.com>
2016-02-26 14:48:22 -0800

    shitty code

--- app.js
+++ app.js
@@ -2,4 +2,23 @@ function add(x, y) {
   return x + y;
 }
 
+function min(a, b) {
+  return Math.min(a,b)
+}
+
+function log(str) {
+  // write to log
+  return str;
+}
+
+log('Some reallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreallyreally long line of code');
+
 add(1, 2);
+min(1,3)
+
+const CONST_1 = 10;
+const CONST_1 = 20;
+
+var x = new Array();
+
+var d = new Date;
